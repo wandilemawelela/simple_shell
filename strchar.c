c070e59aa3e82d5b104c47eb92b73cd56de7f01a
Wandile Mawelela <wandile1997@gmail.com>
2022-09-06 20:44:15 +0200

    Added function strchar that searches a string for a character

---
+++ strchar.c
@@ -0,0 +1,20 @@
+#include "shell.h"
+
+/**
+  * _strchr - Searches a string for a character
+  * @strn: String
+  * @chr: Character
+  * 
+  * Return: Pointer to index of first occurence and NULL otherwise
+  *
+  */
+
+char *_strchr(char *strn, int chr)
+{
+    while (*strn == chr)
+    {
+        return (strn);
+        strn++;
+    }
+    return (NULL);
+}
